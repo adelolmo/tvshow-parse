1650d48a85df7e5179c8dcc9d752701696ca4a67
adelolmo <andoni.delolmo@digitalperformance.de>
2017-07-08 09:40:48 +0200

    support new spanish filename with full words for season and episode.

--- tvshow/parse.go
+++ tvshow/parse.go
@@ -41,7 +41,7 @@ type TvShow struct {
 }
 
 func NewParser() *Parser {
-	rules := make([]rule, 3)
+	rules := make([]rule, 4)
 	rules[0] = rule{
 		Regex:`(^[0-9A-Za-z._ ]*)(^*[Ss][0-9]{2})(^*[Ee][0-9]{2})`,
 		Function:threeGroups,
@@ -53,6 +53,12 @@ func NewParser() *Parser {
 	rules[2] = rule{
 		Regex:`(^[0-9A-Za-z_ ]*)(^*- )(^*[0-9]{1})(^*x)(^*[0-9]{2})`,
 		Function:fiveGroups,
+	}
+	// El Ministerio Del Tiempo Temporada 2 Capitulo 1
+	rules[3] = rule{
+		Regex:`(^[0-9A-Za-z ]*)(^*Temporada [0-9]{1} )(Capitulo [0-9]{1}$)`,
+		//Regex:`(^[0-9A-Za-z ]*)(^*Temporada )(^[0-9]{1} )(Capitulo *$)([0-9]{1}$)`,
+		Function:threeGroupsFullWords,
 	}
 	return &Parser{Rules:rules}
 }
@@ -79,6 +85,37 @@ func threeGroups(filename, regex string) (*TvShow, error) {
 
 	episode := findGroup[3]
 	episodeNumber, err := strconv.Atoi(episode[1:]);
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse episode number from %s", filename)
+	}
+
+	return &TvShow{
+		Name:name,
+		Season:seasonNumber,
+		Episode:episodeNumber}, nil
+}
+
+func threeGroupsFullWords(filename, regex string) (*TvShow, error) {
+	r := regexp.MustCompile(regex)
+	findGroup := r.FindStringSubmatch(filename)
+
+	if (len(findGroup) < 4) {
+		return nil, errors.New("not a match")
+	}
+
+	rawName := findGroup[1]
+
+	escapedName := punctuationReplace.Replace(rawName)
+	name := articleReplace.Replace(strings.Title(strings.TrimSpace(escapedName)))
+
+	season := findGroup[2]
+	seasonNumber, err := strconv.Atoi(strings.Trim(season[10:], " "));
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse season number from %s", filename)
+	}
+
+	episode := findGroup[3]
+	episodeNumber, err := strconv.Atoi(episode[9:]);
 	if err != nil {
 		return nil, fmt.Errorf("Unable to parse episode number from %s", filename)
 	}
--- tvshow/parse_test.go
+++ tvshow/parse_test.go
@@ -92,6 +92,14 @@ func TestDoubleSeasonDigitsDoubleEpisodeDigitsWithBlanks(t *testing.T) {
 	assert.Equal(t, 1, show.Episode)
 }
 
+func TestSpanishSeasonEpisodeFullWords(t *testing.T) {
+	parser := tvshow.NewParser()
+	show, _ := parser.FromFilename("El Ministerio Del Tiempo Temporada 2 Capitulo 1")
+	assert.Equal(t, "El Ministerio del Tiempo", show.Name)
+	assert.Equal(t, 2, show.Season)
+	assert.Equal(t, 1, show.Episode)
+}
+
 func TestFilm(t *testing.T) {
 	parser := tvshow.NewParser()
 	_, err := parser.FromFilename("Logan.2017.1080p.WEB-DL.H264.AC3-EVO[EtHD].mkv")
